6bd778f0de2b29293e55adc7e74d698cc87755bd
braveseokyung <sk100@korea.ac.kr>
2024-10-07 01:25:12 +0900

    mjs 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/popup',
+        permanent: false, // true로 설정하면 301 리다이렉트 (영구 리다이렉트)
+      },
+    ];
+  },
+
   rewrites: async () => {
     return [
       {
@@ -7,24 +17,24 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/py/:path*"
-            : "/api/",
+            : "https://astounding-choux-1a8b7b.netlify.app/api/py/:path*", // 프로덕션 백엔드 주소로 수정
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/py/docs"
-            : "/api/py/docs",
+            : "https://astounding-choux-1a8b7b.netlify.app/api/py/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/py/openapi.json"
-            : "/api/py/openapi.json",
+            : "https://astounding-choux-1a8b7b.netlify.app/api/py/openapi.json",
       },
     ];
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
