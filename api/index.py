b76f89b2311ba2270dd0265b1b83b42c22fc9f99
0gyo <ken107island@gmail.com>
2024-10-06 21:40:25 +0900

    CORS 코드 삭제

--- api/index.py
+++ api/index.py
@@ -38,14 +38,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # 필요한 경우, 특정 도메인만 허용하도록 설정 가능
-    allow_credentials=True,
-    allow_methods=["*"],  # 모든 HTTP 메소드 허용
-    allow_headers=["*"],  # 모든 헤더 허용
-)
-
 @app.get("/api/py/get_db")
 def get_db():
     db = SessionLocal()
